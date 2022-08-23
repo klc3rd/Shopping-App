7c8f3a6a75de49b4fef79059ed605f17302fc197
Kenneth Cluck <klc3rd@tycho.local>
2022-08-23 16:18:09 -0600

    Removed unnecessary import

--- components/page/login/login-form.tsx
+++ components/page/login/login-form.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
 import { useRouter } from "next/router";
 import { signIn } from "next-auth/react";
 import loginText from "../../../utils/text/login-text";
